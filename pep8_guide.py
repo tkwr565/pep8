b3b52fa4c42ed30e8cb2725cf9590260b059f4e7
tkwr565 <tkwr565@gmail.com>
2023-06-15 06:27:26 +0800

    create

---
+++ pep8_guide.py
@@ -0,0 +1,23 @@
+def long_function_name_with_manymanymany_args(
+    arugment1, arugment2, arugment3, arugment4, arugment5, arugment6, arugment7
+):
+    print("on9")
+
+
+def randdddd(one, two):
+    print("nth")
+
+
+# snake case hello_world ### USE THIS FOR VARIABLES
+# camel case helloWorld
+# pascal case HellowWorld
+
+
+# constant variables = NOT CHANGE
+# use all CAPS e.g. COLOR_WORLD
+
+# modules : all lower cases
+
+# functions: snake case
+
+# class camel case
